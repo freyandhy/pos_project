9b77d58a072e21781f4c8494fefb5696e32a01d7
freyandhy <freyandhy@gmail.com>
2015-12-29 18:13:33 +0800

    added migrations files

--- database/migrations/2015_12_29_100937_create_groups_table.php
+++ database/migrations/2015_12_29_100937_create_groups_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGroupsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,14 +12,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('groups', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('username')->unique();
-            $table->string('password', 60);
-            $table->string('type');
-            $table->integer('groups_id');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -31,6 +25,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('groups');
     }
 }
--- database/migrations/2015_12_29_101022_create_items_table.php
+++ database/migrations/2015_12_29_101022_create_items_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,14 +12,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('username')->unique();
-            $table->string('password', 60);
-            $table->string('type');
-            $table->integer('groups_id');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -31,6 +25,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('items');
     }
 }
