1dbb59310088ff7ef8181172f3a070369f7b821e
freyandhy <freyandhy@gmail.com>
2015-12-30 16:51:20 +0800

    added Admin LTE template

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', 'Auth\AuthController@getLogin');
+Route::get('/', 'DashboardController@index');
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 /*
 |--------------------------------------------------------------------------
